7c42172cfcfc24a711bdde785f2383279670c85b
Little Panda <>
2022-04-03 18:41:14 -0600

    Convert all handling quality and sellin of each type of item into functions

--- gilded_rose.py
+++ gilded_rose.py
@@ -6,11 +6,40 @@ class GildedRose(object):
     def is_normal_item(self, item):
         return item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros"
 
+    def handle_quality_normal(self, item):
+        item.quality = max(0, item.quality - 1)
+
+    def handle_sellin_normal(self, item):
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = max(0, item.quality - 1)
+
     def is_aged_brie(self, item):
         return item.name == "Aged Brie"
 
+    def handle_quality_aged_brie(self, item):
+        item.quality = min(50, item.quality + 1)
+
+    def handle_sellin_aged_brie(self, item):
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = min(50, item.quality + 1)
+
     def is_backstage(self, item):
         return item.name == "Backstage passes to a TAFKAL80ETC concert"
+
+    def handle_quality_backstage(self, item):
+        item.quality = item.quality + 1
+        if item.sell_in < 11:
+            item.quality = item.quality + 1
+        if item.sell_in < 6:
+            item.quality = item.quality + 1
+        item.quality = min(50, item.quality)
+
+    def handle_sellin_backstage(self, item):
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = 0
 
     def is_sulfuras(self, item):
         return item.name == "Sulfuras, Hand of Ragnaros"
@@ -20,42 +49,20 @@ class GildedRose(object):
             # Sulfuras quality never change
             # Item's not Aged Brie or Backstage will decrease its quality by 1 each day
             if self.is_normal_item(item):
-                item.quality = max(0, item.quality - 1)
+                self.handle_quality_normal(item)
+                self.handle_sellin_normal(item)
 
             if self.is_aged_brie(item):
-                item.quality = min(50, item.quality + 1)
+                self.handle_quality_aged_brie(item)
+                self.handle_sellin_aged_brie(item)
 
             # Item's Aged Brie or Backstage
             if self.is_backstage(item):
-                item.quality = item.quality + 1
-                if item.sell_in < 11:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    item.quality = item.quality + 1
-                item.quality = min(50, item.quality)
+                self.handle_quality_backstage(item)
+                self.handle_sellin_backstage(item)
 
             if self.is_sulfuras(item):
                 continue
-
-            # After dealing with quality, we'll handle sell in
-            # If it's not sulfuras, its sell in date will decrease by one
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            # If it's passed the sell in date, normal item's quality will decrease by another 1 (2)
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                # If it's normal item and sell in date has passed, its quality decrease by 2 (1 more)
-                                item.quality = item.quality - 1
-                    # If it's passed Backstage concert, item quality will be 0
-                    else:
-                        item.quality = item.quality - item.quality
-                # If it's Aged Brie, it's quality keep getting better (twice as fast for increase and decrease?)
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
 
 
 class Item:
